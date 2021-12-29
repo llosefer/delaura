a021bcf586c402815d599c5d831266119fde58b0
llosefer <96617965+llosefer@users.noreply.github.com>
2021-12-30 00:30:36 +0100

    Update main.js
    sustituidas las imánes por 2 que he subido a la carpeta images

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let miImage = document.querySelector('img');
 miImage.onclick = function () {
     let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/firefox-icon.png') {
-      miImage.setAttribute('src','images/firefox2.png');
+    if (miSrc === 'images/a.jpg') {
+      miImage.setAttribute('src','images/b.jpg');
     } else {
-      miImage.setAttribute('src', 'images/firefox-icon.png');
+      miImage.setAttribute('src', 'images/a.jpg');
     }
 }
 
